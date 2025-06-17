bf24e41ea7503b9bbd2f32bcd62407b6919fc895
BrunoRigoni <brigoni2011@gmail.com>
2025-06-17 11:01:04 -0300

    dicionários em python.

--- estoque.py
+++ estoque.py
@@ -23,16 +23,20 @@ while True:
 
     if option == 1:
 
-        novo_produto = input("Digite o produto que deseja adicionar: ").lower()
+        novo_produto = input(
+            "Digite o produto que deseja adicionar: ").strip().lower()
         estoque.append(novo_produto)
-        mostrar_produto()
+        print(f"O produto '{novo_produto}' foi adicionado com sucesso!")
 
     elif option == 2:
         remover_produto = input(
-            "DIGITE O PRODUTO QUE DESEJA REMOVER: ").lower()
+            "DIGITE O PRODUTO QUE DESEJA REMOVER: ").strip().lower()
         if remover_produto in estoque:
             estoque.remove(remover_produto)
-            print(f"O produto {remover_produto} foi adicionado com sucesso!")
+            print(f"O produto {remover_produto} foi removido com sucesso!")
+        else:
+            print(
+                f"Produto {remover_produto} não existe no estoque, digite um produto válido.")
 
     elif option == 3:
         mostrar_produto()
