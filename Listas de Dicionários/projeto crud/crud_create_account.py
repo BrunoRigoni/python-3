c78f1608205d8ac35c725ce482dbb0100de39906
BrunoRigoni <brigoni2011@gmail.com>
2025-06-18 20:55:11 -0300

    fix.

--- Listas de Dicionários/projeto crud/crud_create_account.py
+++ Listas de Dicionários/projeto crud/crud_create_account.py
@@ -14,12 +14,10 @@ def register_new_user():
     new_user_id = random.randint(1, 1000)
     new_user_name = input("Digite o nome de usuario: ")
 
-    print("Digite um valor correto.")
-
     for user in users:
         if user[name] == new_user_name:
             print("Usuario ja existe.")
-        return
+            return
     try:
         new_password = getpass.getpass("Digite a senha do usuario: ")
         confirm_password = getpass.getpass("Confirme a senha do usuario: ")
@@ -49,6 +47,8 @@ def show_all_users():
 
 
 def remove_user():
+    for user in users:
+        print(f"ID: {user["id"]} -> nome: {user["name"]}")
 
     try:
         user_remove_id = int(
@@ -81,7 +81,7 @@ while option != 5:
     sep("")
     sep("-")
     print("[1] CADASTRO DE NOVO USUÁRIO")
-    print("[2] CONSULTAR PERFIL")
+    print("[2] CONSULTAR CADASTROS")
     print("[3] ALTERAR SENHA")
     print("[4] DELETAR USUARIO")
     print("[5] SAIR")
@@ -111,7 +111,7 @@ while option != 5:
         print("ALTERAR SENHA")
     elif option == 4:
         remove_user()
-        print("CADASTRO DE USUARIO")
+        input("ENTER PARA VOLTAR AO MENÚ PRINCIPAL.")
     elif option == 5:
         print("APLICAÇÃO ENCERRADA.")
 
