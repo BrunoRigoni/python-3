6ebc5720fc31d79f8f77232c92adeb0e5b8d4414
BrunoRigoni <brigoni2011@gmail.com>
2025-06-26 14:48:26 -0300

    adicionando nomes a partir de um menu interativo.

--- SQLITE/SQLite aula/aula_sqlite1.py
+++ SQLITE/SQLite aula/aula_sqlite1.py
@@ -1,4 +1,14 @@
 import sqlite3
+import os
+
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def pause():
+    input("Enter para voltar.")
+
 
 banco = sqlite3.connect('primeiro_banco.db')
 
@@ -7,6 +17,31 @@ cursor = banco.cursor()
 
 # cursor.execute("CREATE TABLE pessoas (nome text, idade integer, email text)")
 
-cursor.execute("INSERT INTO pessoas VALUES('Bruno',24,'bruno@mail.com')")
 
-banco.commit()
+def inserir_nova_pessoa():
+    nome = input("Nome: ")
+    idade = int(input('Idade: '))
+    mail = input("Email: ")
+    cursor.execute(f"INSERT INTO pessoas VALUES('{nome}',{idade},'{mail}')")
+    banco.commit()
+
+
+while True:
+    try:
+        option = int(input("[1] Adicionar pessoa.| [2] Sair."))
+    except ValueError:
+        print("Digite uma opção válida.")
+        continue
+
+    if option == 1:
+        clear()
+        inserir_nova_pessoa()
+        pause()
+        clear()
+    elif option == 2:
+        clear()
+        print('Saindo...')
+        quit()
+    else:
+        print("Opção inválida.")
+        pause()
